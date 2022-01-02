e44f4a397a55764e0230f17dda2e282044a2ac35
XimenZangief <s9671073@gmail.com>
2022-01-02 20:47:15 +0800

    完成update_show和update功能

--- app2/api/update.php
+++ app2/api/update.php
@@ -2,4 +2,11 @@
 //處理更新資料的請求
 include_once "base.php";
 
+$stu=$Stu->find($_POST['id']);
+$stu['name']=$_POST['name'];
+$stu['classroom']=$_POST['classroom'];
+$stu['birthday']=$_POST['birthday'];
+$stu['seat_num']=$_POST['seat_num'];
+
+$Stu->save($stu);
 ?>
