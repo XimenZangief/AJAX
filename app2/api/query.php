d31e500c8316e6e119d3c74879bdef587b56148d
XimenZangief <s9671073@gmail.com>
2022-01-02 19:21:07 +0800

    完成query和del功能

--- app2/api/query.php
+++ app2/api/query.php
@@ -2,4 +2,9 @@
 //處理查詢資料的請求
 include_once "base.php";
 
+$q=$_GET['q'];
+// %$q%表示$q是模糊查詢，$q左右皆可用有其他字串
+// $q=台北，%$q=XXX台北，$q%=台北XXX，%$q%=XXX台北XXX
+$rows=$Stu->q("SELECT * from `students` where `name` like '%$q%'");
+echo json_encode($rows);
 ?>
